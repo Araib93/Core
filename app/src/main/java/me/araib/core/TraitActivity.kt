8b5a66b69dedd07078f67cbd4b16e1cc4531c080
Araib Shafiq <m.araib.shafiq@gmail.com>
2019-09-30 13:27:31 +0500

    - Trigger initTraits on creating TraitActivity

--- app/src/main/java/me/araib/core/TraitActivity.kt
+++ app/src/main/java/me/araib/core/TraitActivity.kt
@@ -1,8 +1,14 @@
 package me.araib.core
 
 import android.content.Intent
+import android.os.Bundle
 
 abstract class TraitActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initTraits()
+    }
+
     abstract fun initTraits()
 
     abstract fun handleTraitResult(requestCode: Int, resultCode: Int, data: Intent?)
